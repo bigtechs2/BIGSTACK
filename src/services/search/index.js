9610c7152100707be76cbc724b019d6dd9ff84ae
bigmanjtech <bigmanj429@gmail.com>
2026-09-22 09:00:05 +0300

    Create index.js

---
+++ src/services/search/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    applesearch: require("./applesearch.service"),
+    imagesearch: require("./imagesearch.service"),
+    happymod: require("./happymod.service"),
+    lyrics: require("./lyrics.service"),
+    spotifylyric: require("./spotifylyric.service"),
+    spotifysearch: require("./spotifysearch.service"),
+    pinterestsearch: require("./pinterestsearch.service"),
+    youtubesearch: require("./youtubesearch.service")
+};
