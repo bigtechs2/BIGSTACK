b1975098594d5007d02e95cb8cf4f83a6de81852
bigmanjtech <bigmanj429@gmail.com>
2026-09-22 08:59:35 +0300

    Create index.js

---
+++ src/commands/search/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    applesearch: require("./applesearch"),
+    imagesearch: require("./imagesearch"),
+    happymod: require("./happymod"),
+    lyrics: require("./lyrics"),
+    spotifylyric: require("./spotifylyric"),
+    spotifysearch: require("./spotifysearch"),
+    pinterestsearch: require("./pinterestsearch"),
+    youtubesearch: require("./youtubesearch")
+};
