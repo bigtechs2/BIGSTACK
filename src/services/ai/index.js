950dafb657398042226577209b54a8a241adeaf9
bigmanjtech <bigmanj429@gmail.com>
2026-09-26 09:37:56 +0300

    Create index.js

---
+++ src/services/ai/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    ai: require("./ai.service"),
+    image: require("./image.service"),
+    vision: require("./vision.service"),
+    voice: require("./voice.service"),
+    music: require("./music.service"),
+    session: require("./session.service")
+};
